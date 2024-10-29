d0f9bd1c580d2760b70560b83c72ae8815e54512
akshayanpuvi <akshayan.puvichandran@student.uts.edu.au>
2024-10-29 22:34:52 +1100

    Single Player & Start Up

--- StartUp.m
+++ StartUp.m
@@ -1,28 +1,29 @@
+
 function StartUp
-% Create a new figure window for the GUI
-screenSize = get(0, 'ScreenSize');
-figWidth = 400;
-figHeight = 200;
+    % Create a new figure window for the GUI
+    screenSize = get(0, 'ScreenSize');
+    figWidth = 400;
+    figHeight = 200;
 
-figX = (screenSize(3) - figWidth) / 2;
-figY = (screenSize(4) - figHeight) / 2;
+    figX = (screenSize(3) - figWidth) / 2;
+    figY = (screenSize(4) - figHeight) / 2;
 
-hFigure = figure('Position', [figX, figY, figWidth, figHeight], ...
-    'Name', 'Connect Four Start-Up', ...
-    'NumberTitle', 'off', ...
-    'Color', [0.8, 0.9, 1], ...
-    'Resize', 'off');
+    hFigure = figure('Position', [figX, figY, figWidth, figHeight], ...
+        'Name', 'Connect Four Start-Up', ...
+        'NumberTitle', 'off', ...
+        'Color', [0.8, 0.9, 1], ...
+        'Resize', 'off');
 
-% Add a title to the GUI
-uicontrol('Style', 'text', 'String', 'Welcome to Connect Four!', ...
-    'Position', [50, 130, 300, 40], 'FontSize', 14, 'FontWeight', 'bold', ...
-    'BackgroundColor', [0.8, 0.9, 1], 'HorizontalAlignment', 'center');
+    % Add a title to the GUI
+    uicontrol('Style', 'text', 'String', 'Welcome to Connect Four!', ...
+        'Position', [50, 130, 300, 40], 'FontSize', 14, 'FontWeight', 'bold', ...
+        'BackgroundColor', [0.8, 0.9, 1], 'HorizontalAlignment', 'center');
 
-% Add the "PLAY NOW" button
-uicontrol('Style', 'pushbutton', 'String', 'PLAY NOW', ...
-    'Position', [150, 50, 100, 50], 'FontSize', 12, 'FontWeight', 'bold', ...
-    'BackgroundColor', [0.2, 0.6, 0.8], 'ForegroundColor', [1, 1, 1], ...
-    'Callback', @playNowCallback);
+    % Add the "PLAY NOW" button
+    uicontrol('Style', 'pushbutton', 'String', 'PLAY NOW', ...
+        'Position', [150, 50, 100, 50], 'FontSize', 12, 'FontWeight', 'bold', ...
+        'BackgroundColor', [0.2, 0.6, 0.8], 'ForegroundColor', [1, 1, 1], ...
+        'Callback', @playNowCallback);
 
 % Callback function for the "PLAY NOW" button
 function playNowCallback(~, ~)
@@ -33,14 +34,19 @@ function playNowCallback(~, ~)
         case 'Robot Masterclass'
             disp('Starting Robot Masterclass (Two Robots Playing)...');
             close(hFigure);
-            % Launch Robot vs Robot GUI here
-            % RobotMasterclass;  % Uncomment this line when ready
-
+            
+            % Create a new figure for final2 to run in
+            hFinal2Figure = figure('Name', 'Robot Masterclass', ...
+                                   'NumberTitle', 'off', ...
+                                   'Color', [0.9, 0.9, 0.9]);
+            
+            % Run final2 in the base workspace
+            evalin('base', 'final2');
+            
         case 'Single-Player'
             disp('Starting Single-Player (Human vs Robot)...');
             close(hFigure);
-            % Launch Single-Player GUI here
-            % SinglePlayer;  % Uncomment this line when ready
+            evalin('base', 'SinglePlayer');  % Run SinglePlayer in the base workspace
 
         case 'Cancel'
             disp('Action canceled.');
