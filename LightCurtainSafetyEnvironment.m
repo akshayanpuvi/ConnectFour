757753de917421f5dfa566f3a24825e2503173d9
akshayanpuvi <akshayan.puvichandran@student.uts.edu.au>
2024-10-29 20:46:03 +1100

    Update

--- LightCurtainSafetyEnvironment.m
+++ LightCurtainSafetyEnvironment.m
@@ -3,4 +3,15 @@ classdef LightCurtainSafety < handle
         safetyZone % Safety zone boundary
         objectsToMonitor % List of objects that need monitoring
     end
-end
+    
+    methods
+        function self = LightCurtainSafety()
+            % Define the safety zone as a rectangular region in the x-y plane
+            self.safetyZone = [-1.5, 1.5; -1.5, 1.5; 0, 3.5]; % [xmin xmax; ymin ymax; zmin zmax]
+            self.objectsToMonitor = {};
+        end
+        
+        function addObjectToMonitor(self, objectHandle)
+            % Add an object handle to the list of monitored objects
+            self.objectsToMonitor{end+1} = objectHandle;
+        end
